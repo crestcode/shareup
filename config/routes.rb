1b3b3388a0ef2351400e8e7e647ec83730b7f7ad
John Kasiewicz <crestcode@gmail.com>
2014-03-03 13:03:16 -0500

    testing welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Shareup::Application.routes.draw do
-  devise_for :users
+  namespace :api do
+    resources :shares
+  end
+
+  get'/dashboard'=>'welcome#dashboard'
+  root to:'welcome#index'
+
+  devise_for :users,
+             :controllers => {
+                 :omniauth_callbacks => "users/omniauth_callbacks"
+             }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
